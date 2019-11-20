5dad8f5e9d2ee4844e0d7edb868f7eae03fec58b
cbush19 <cbush5400@gmail.com>
2019-11-20 15:47:01 -0500

    update delete processing

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var fs = require('fs');
 
 // Sets up the Express App
 // =============================================================
@@ -13,54 +14,60 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+const db = require('./db.json');
 // Routes
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-    // res.send("Welcome to the Star Wars Page!")
-    console.log('path is this:'+path + 'dirname is this:'+__dirname);
-    res.sendFile(path.join(__dirname, "index.html"));
-  });  
+
+  app.get("/", function(req, res) {
+    res.json(path.join(__dirname, "/public/index.html"));
+  });
 
   app.get("/notes", function(req, res) {
     // res.send("Welcome to the Star Wars Page!")
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
   });
   
   // Displays all characters
   app.get("/api/notes", function(req, res) {
-    return res.json({
-        routeName: "yoda",
-        name: "Yoda",
-        role: "Jedi ffffffffMaster",
-        age: 900,
-        forcePoints: 2000
-      });
+    return res.json(db);
   });
-  
   
 // Create New Characters - takes in JSON input
 app.post("/api/notes", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newNote = req.body;
-  
-    // Using a RegEx Pattern to remove spaces from newNote
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
-  
+    newNote.id = parseInt(db[db.length-1].id)+1;
+  // crbtemp may need to add an id to the note here, notearray.lenght will suffice
     console.log(newNote);
-  
-    characters.push(newNote);
+    db.push(newNote);
+   
+    fs.writeFile('db.json', JSON.stringify(db), (suc, err) => {
+      if (err) { 
+        return console.log(err);
+      }
+    });
   
     res.json(newNote);
   });
   
+  app.delete('/api/notes/:id',(req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const noteDelete = db.findIndex(element => parseInt(element.id)=== parseInt(id));
+    db.splice(noteDelete, 1);
+    fs.writeFile('db.json', JSON.stringify(db), (suc, err) => {
+      if (err) { 
+        return console.log(err);
+      }
+      res.sendStatus(200);
+    });
+  });
   
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-  
